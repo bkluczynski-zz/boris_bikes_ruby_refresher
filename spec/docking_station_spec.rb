e0e1826ed29ea71e834cb05efb64e60147f0686d
Bart <kluczynskib@gmail.com>
2017-11-02 11:23:09 +0000

    changed bike class instances into doubles with mocks

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,18 +1,18 @@
 require "docking_station"
 
 describe DockingStation do
+
+  let(:bike) { double :bike }
+
   it { is_expected.to respond_to :release_bike }
 
   it "releases a bike when there is a bike available" do
-    bike = Bike.new
-    station = DockingStation.new
-    station.dock(bike)
-    station.release_bike(bike)
-    expect(bike).to be_working
+    allow(bike).to receive(:broken?).and_return(false)
+    subject.dock(bike)
+    expect(subject.release_bike(bike)).to be bike
   end
 
   it 'docks something' do
-    bike = Bike.new
     # We want to return the bike we dock
     expect(subject.dock(bike)).to eq [bike]
   end
@@ -22,21 +22,24 @@ describe DockingStation do
   end
 
   it 'raises error when there is no bikes available' do
-    expect { subject.release_bike(Bike.new) }.to raise_error("Sorry, there are no bikes available for hire")
+    expect { subject.release_bike(bike) }.to raise_error("Sorry, there are no bikes available for hire")
   end
 
   it 'has a default capacity of 20 bikes' do
-    DockingStation::DEFAULT_CAPACITY.times { subject.dock Bike.new }
-    expect { subject.dock(Bike.new) }.to raise_error("Sorry, there are no free slots to dock a bike")
+    DockingStation::DEFAULT_CAPACITY.times { subject.dock bike }
+    expect { subject.dock(bike) }.to raise_error("Sorry, there are no free slots to dock a bike")
   end
 
   it 'does not release broken bike' do
-    bike = Bike.new
-    bike.report_broken
+    allow(bike).to receive(:broken?).and_return(true)
     subject.dock(bike)
     expect { subject.release_bike(bike) }.to raise_error("Sorry, this bike is broken")
   end
 
+  it 'can accept a broken bike' do
+    allow(bike).to receive(:broken?).and_return(true)
+    expect(subject.dock(bike)).to eq [bike]
+  end
 
 
 end
